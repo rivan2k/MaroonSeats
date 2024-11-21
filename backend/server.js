ef0c0d13cf0a599d54750e77d00c538578de1c77
rivan2k <rivanadhikari@gmail.com>
2024-11-20 20:55:10 -0600

    base backend built for generic product

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js'
+import productRoutes from "./routes/product.js"
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+
+
+
+// console.log(process.env.MONGO_URI);
+
+app.listen(PORT, () =>{
+    connectDB();
+    console.log("Server started at http://localhost:" + PORT);
+});
+
+
