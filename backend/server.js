a66255cd4408e6638172c247c2e12dde3cfef4c9
rivan2k <rivanadhikari@gmail.com>
2025-09-05 10:31:48 -0500

    updated cors so that the frontend could interact with backend

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    credentials: true, 
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true
 }));
 
 
